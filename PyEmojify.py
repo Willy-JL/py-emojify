1bf7258d6113328943514996912bce2a0c683b5c
WillyJL <willy.leslie@icloud.com>
2020-12-01 21:43:12 +0100

    Multi line fix

--- PyEmojify.py
+++ PyEmojify.py
@@ -5,13 +5,16 @@ with open('emojimap.json', encoding='utf-8') as f:
 
 
 def emojify(text: str, position='after'):
-    words = text.split()
-    for i, word in enumerate(words):
-        for emoji in emoji_map:
-            if word.lower() in emoji_map[emoji]:
-                if position == 'after':
-                    words[i] = f'{word} {emoji}'
-                elif position == 'before':
-                    words[i] = f'{emoji} {word}'
-                break
-    return ' '.join(words)
+    lines = text.split('\n')
+    for i, line in enumerate(lines):
+        words = line.split()
+        for i, word in enumerate(words):
+            for emoji in emoji_map:
+                if word.lower() in emoji_map[emoji]:
+                    if position == 'after':
+                        words[i] = f'{word} {emoji}'
+                    elif position == 'before':
+                        words[i] = f'{emoji} {word}'
+                    break
+        lines[i] = ' '.join(words)
+    return '\n'.join(lines)
